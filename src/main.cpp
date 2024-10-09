daa9d3335e3a3eaf5894aaac9866db3e5a8f4f69
yongjer <jerjer920813@gmail.com>
2024-10-09 11:10:56 +0900

    Refactor motor control code for improved functionality

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,40 @@
 
 const int leftMotorPin1 = 2;
 const int leftMotorPin2 = 3;
-
-// test code
+const int rightMotorPin1 = 4;
+const int rightMotorPin2 = 5;
 
 void setup() {
   pinMode(leftMotorPin1, OUTPUT);
   pinMode(leftMotorPin2, OUTPUT);
+  pinMode(rightMotorPin1, OUTPUT);
+  pinMode(rightMotorPin2, OUTPUT);
 }
 
+// test the motors to any direction
 void loop() {
+  // go straight
   digitalWrite(leftMotorPin1, HIGH);
   digitalWrite(leftMotorPin2, LOW);
-  delay(1000);
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+  delay(2000);
+  // go back
   digitalWrite(leftMotorPin1, LOW);
   digitalWrite(leftMotorPin2, HIGH);
-  delay(1000);
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, HIGH);
+  delay(2000);
+  // turn left
+  digitalWrite(leftMotorPin1, LOW);
+  digitalWrite(leftMotorPin2, HIGH);
+  digitalWrite(rightMotorPin1, HIGH);
+  digitalWrite(rightMotorPin2, LOW);
+  delay(2000);
+  // turn right
+  digitalWrite(leftMotorPin1, HIGH);
+  digitalWrite(leftMotorPin2, LOW);
+  digitalWrite(rightMotorPin1, LOW);
+  digitalWrite(rightMotorPin2, HIGH);
+  delay(2000);
 }
